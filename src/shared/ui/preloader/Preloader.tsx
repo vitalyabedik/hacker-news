8f8dd13bafafc163d586e1ee3c2b380264d04352
Vitali Bedik <vitalya.bedik@gmail.com>
2023-12-24 00:32:34 +0300

    feat: add variants and styles for Preloader

--- src/shared/ui/preloader/Preloader.tsx
+++ src/shared/ui/preloader/Preloader.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 
-export const Preloader: React.FC = () => {
+type VariantType = 'default' | 'small'
+
+type Props = {
+  isFullScreen?: boolean
+  variant?: VariantType
+}
+
+export const Preloader: React.FC<Props> = ({ isFullScreen = true, variant = 'default' }) => {
+  const preloaderSize = variant === 'default' ? '40' : '20'
+  const isFullScreenHeight = isFullScreen ? 'h-screen' : 'mt-16'
+  const hasMarginTop = variant === 'small' ? 'mt-8' : ''
+
   return (
-    <div className={'flex items-center justify-center h-screen'}>
+    <div className={`flex items-center justify-center ${isFullScreenHeight} ${hasMarginTop}`}>
       <div
-        className={'w-40 h-40 border-t-4 border-b-4 border-indigo-500 rounded-full animate-spin'}
+        className={`w-${preloaderSize} h-${preloaderSize} border-t-4 border-b-4 border-indigo-500 rounded-full animate-spin`}
       ></div>
     </div>
   )
