b41b111a2608eb733035daccd092af92263be334
Vitali Bedik <vitalya.bedik@gmail.com>
2023-12-25 13:53:00 +0300

    refactor: change styles for not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,13 +6,13 @@ import Link from 'next/link'
 const NotFound = () => {
   return (
     <PageWrapper>
-      <main className={'grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8'}>
+      <main className={'grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8'}>
         <div className={'text-center'}>
           <p className={'text-base font-semibold text-indigo-600'}>404</p>
-          <h1 className={'mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl'}>
+          <h1 className={'mt-4 text-3xl font-bold tracking-tight text-white sm:text-5xl'}>
             Page not found
           </h1>
-          <p className={'mt-6 text-base leading-7 text-gray-600'}>
+          <p className={'mt-6 text-base leading-7 text-white'}>
             Sorry, we couldn’t find the page you’re looking for.
           </p>
           <div className={'mt-10 flex items-center justify-center gap-x-6'}>
